dfffb182733e402e47c8639fd3b4bedbb5407296
Sebastien Chabrol <schabrol@lafourchette.com>
2018-03-13 15:04:38 +0100

    Add arme for RPG. Don't forget to add it on project ;)

--- draft/personnage.cpp
+++ draft/personnage.cpp
@@ -1,29 +1,45 @@
+
 #include "personnage.h"
 
+
 using namespace std;
+
+
+Personnage::Personnage() : m_vie(100), m_mana(100)
+{
+
+}
+
+Personnage::Personnage(string nomArme, int degatsArme) : m_vie(100), m_mana(100), m_arme(nomArme, degatsArme)
+{
+
+}
+
+Personnage::~Personnage()
+{
+
+}
 
 void Personnage::recevoirDegats(int nbDegats)
 {
     m_vie -= nbDegats;
-    //On enlève le nombre de dégâts reçus à la vie du personnage
 
-    if (m_vie < 0) //Pour éviter d'avoir une vie négative
+    if (m_vie < 0)
     {
-        m_vie = 0; //On met la vie à 0 (cela veut dire mort)
+        m_vie = 0;
     }
 }
 
 void Personnage::attaquer(Personnage &cible)
 {
-    cible.recevoirDegats(m_degatsArme);
-    //On inflige à la cible les dégâts que cause notre arme
+    cible.recevoirDegats(m_arme.getDegats());
 }
 
 void Personnage::boirePotionDeVie(int quantitePotion)
 {
     m_vie += quantitePotion;
 
-    if (m_vie > 100) //Interdiction de dépasser 100 de vie
+    if (m_vie > 100)
     {
         m_vie = 100;
     }
@@ -31,12 +47,24 @@ void Personnage::boirePotionDeVie(int quantitePotion)
 
 void Personnage::changerArme(string nomNouvelleArme, int degatsNouvelleArme)
 {
-    m_nomArme = nomNouvelleArme;
-    m_degatsArme = degatsNouvelleArme;
+    m_arme.changer(nomNouvelleArme, degatsNouvelleArme);
 }
 
 bool Personnage::estVivant()
 {
-    return m_vie > 0;
+    if (m_vie > 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
+void Personnage::afficherEtat()
+{
+    cout << "Vie : " << m_vie << endl;
+    cout << "Mana : " << m_mana << endl;
+    m_arme.afficher();
+}
--- draft/personnage.h
+++ draft/personnage.h
@@ -1,30 +1,29 @@
-#ifndef PERSONNAGE_H
-#define PERSONNAGE_H
-
+#ifndef DEF_PERSONNAGE
+#define DEF_PERSONNAGE
 
 #include <iostream>
 #include <string>
+#include "arme.h"
 
 class Personnage
-
 {
-
     public:
 
-        void recevoirDegats(int nbDegats);
-        void attaquer(Personnage &cible);
-        void boirePotionDeVie(int quantitePotion);
-        void changerArme(std::string nomNouvelleArme, int degatsNouvelleArme);
-        bool estVivant();
-
+    Personnage();
+    Personnage(std::string nomArme, int degatsArme);
+    ~Personnage();
+    void recevoirDegats(int nbDegats);
+    void attaquer(Personnage &cible);
+    void boirePotionDeVie(int quantitePotion);
+    void changerArme(std::string nomNouvelleArme, int degatsNouvelleArme);
+    bool estVivant();
+    void afficherEtat();
 
     private:
 
-        int m_vie;
-        int m_mana;
-        std::string m_nomArme; //Pas de using namespace std, il faut donc mettrestd:: devant string
-        int m_degatsArme;
-
+    int m_vie;
+    int m_mana;
+    Arme m_arme;
 };
 
-#endif // PERSONNAGE_H
+#endif
